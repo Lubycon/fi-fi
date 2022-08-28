8714a72c045fdbc354e5659026919bb13cba9084
evan-moon <bboydart91@gmail.com>
2022-08-28 23:40:26 +0900

    add ld+json

--- src/components/CommonHead/index.tsx
+++ src/components/CommonHead/index.tsx
@@ -5,6 +5,27 @@ interface Props {
 }
 
 const logoPath = '/favicon.ico';
+const title = '내 연봉, 실수령액 알아보기';
+const description = '내 연봉, 실수령액은 얼마일까? 국세청 통계를 기반으로 정확하게 알아보자!';
+const author = 'Double Tap';
+
+const siteData = {
+  '@context': 'https://schema.org',
+  '@type': 'WebPage',
+  name: title,
+  url: 'https://salary.double-tap.io',
+  mainEntityOfPage: 'https://salary.double-tap.io',
+  description,
+  author: {
+    '@type': 'Organization',
+    name: author,
+  },
+  creator: {
+    '@type': 'Organization',
+    name: author,
+  },
+};
+
 function CommonHead({ container: Container = Fragment }: Props) {
   return (
     <Container>
@@ -13,11 +34,11 @@ function CommonHead({ container: Container = Fragment }: Props) {
         name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, viewport-fit=cover"
       />
-      <title>연봉 계산기</title>
-      <meta name="author" content="Double Tap" />
+      <title>{title}</title>
+      <meta name="author" content={author} />
       <link rel="shortcut icon" href={logoPath} />
-      <meta property="og:title" content="연봉계산기" />
-      <meta property="og:description" content="연봉계산기" />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
       <meta property="og:image" content="/image/jumbotron.png" />
       <link rel="icon" type="image/png" sizes="16x16" href={logoPath} />
       <link rel="icon" type="image/png" sizes="32x32" href={logoPath} />
@@ -31,6 +52,7 @@ function CommonHead({ container: Container = Fragment }: Props) {
       <link rel="apple-touch-icon" sizes="152x152" href={logoPath} />
       <meta name="msapplication-TileImage" content={logoPath} />
       <meta name="google-site-verification" content="MLGj_VaV04VWUEkIcsb1Ov-tYu9bcp2TBakfflmc4DA" />
+      <script type="application/ld+json">{JSON.stringify(siteData)}</script>
     </Container>
   );
 }
