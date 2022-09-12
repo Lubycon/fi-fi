ce258fb670caa687ff9e6b1023c178f9394e4709
evan-moon <bboydart91@gmail.com>
2022-09-13 00:15:09 +0900

    컴포넌트 정리

--- src/image-converter/pages/HomePage/index.tsx
+++ src/image-converter/pages/HomePage/index.tsx
@@ -1,14 +1,18 @@
-import Uploader from 'image-converter/components/files/uploader';
+import Uploader from 'image-converter/components/Uploader';
 import ServiceHead from 'image-converter/components/ServiceHead';
+import Footer from 'image-converter/components/Footer';
+import Top from 'image-converter/components/Top';
 import 'semantic-ui-css/semantic.min.css';
 
 export default function Home() {
   return (
     <div>
+      <Top />
       <ServiceHead />
       <main>
         <Uploader />
       </main>
+      <Footer />
     </div>
   );
 }
