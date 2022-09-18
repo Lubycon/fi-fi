fc58c57ad3b9db1c9f9aa1ddb431e52eb1280e0e
DongHyun Hwang <dhhwang@toss.im>
2022-09-19 00:33:46 +0900

    refactor: class component to functional component
     Refs:

--- src/converters/image-converter/components/Uploader.jsx
+++ src/converters/image-converter/components/Uploader.jsx
@@ -1,18 +1,12 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import { Button, Segment, Divider, Tab, Message, Form, Icon } from 'semantic-ui-react';
 
-class Uploader extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { imageLink: '' };
-  }
-
-  onFormSubmit = e => {
+const Uploader = () => {
+  const [imageInfo, setImageInfo] = useState({ imageBlob: '', imageName: '', imageLink: '' });
+  const onFormSubmit = e => {
     e.preventDefault();
   };
-
-  fileChange = fileChangeEvent => {
-    const fileChangeScope = this;
+  const fileChange = fileChangeEvent => {
     const imageBlob = fileChangeEvent.target.files[0];
     const imageName = fileChangeEvent.target.files[0].name.split('.')[0];
 
@@ -23,7 +17,6 @@ class Uploader extends Component {
 
     const reader = new FileReader();
     reader.readAsDataURL(imageBlob);
-
     reader.onloadend = function (reloadedEvent) {
       const image = new Image();
       image.src = reloadedEvent.target.result;
@@ -42,51 +35,49 @@ class Uploader extends Component {
         const imageLink = document.createElement('a');
         imageLink.download = imageName;
         imageLink.href = convertedJPEGImage;
-        fileChangeScope.setState({ imageLink: imageLink });
+        setImageInfo({ imageBlob: imageBlob, imageName: imageName, imageLink: imageLink });
       };
     };
   };
-
-  fileDownload = async () => {
-    this.state.imageLink.click();
+  const fileDownload = async () => {
+    imageInfo.imageLink.click();
   };
-
-  render() {
-    const panes = [
-      {
-        menuItem: 'PNG to JPG',
-        render: () => (
-          <Tab.Pane attached={false}>
-            <Message>변환할 이미지를 선택하시고, &quot;Convert&quot; 버튼을 눌러주세요.</Message>
-            <Form onSubmit={this.onFormSubmit}>
-              <Form.Field>
-                <label>File Upload</label>
-                <Button as="label" htmlFor="file" type="button" animated="fade">
-                  <Button.Content visible>
-                    <Icon name="file" />
-                  </Button.Content>
-                  <Button.Content hidden>Choose a File</Button.Content>
-                </Button>
-                <input type="file" id="file" hidden onChange={this.fileChange} />
-                <Button style={{ marginTop: '20px' }} type="submit" id="Download" onClick={this.fileDownload}>
-                  Download
-                </Button>
-              </Form.Field>
-            </Form>
-            <div>
-              <canvas id="canvasInput"> input</canvas>
-            </div>
-          </Tab.Pane>
-        ),
-      },
-    ];
-    return (
-      <Segment style={{ padding: '5em 1em' }} vertical>
-        <Divider horizontal>Image Converter</Divider>
-        <Tab menu={{ pointing: true }} panes={panes} />
-      </Segment>
-    );
-  }
-}
+  return (
+    <Segment style={{ padding: '5em 1em' }} vertical>
+      <Divider horizontal> Image Converter</Divider>
+      <Tab
+        menu={{ pointing: true }}
+        panes={[
+          {
+            menuItem: 'PNG to JPG',
+            render: () => (
+              <Tab.Pane attached={false}>
+                <Message>변환할 이미지를 선택하시고, &quot;Convert&quot; 버튼을 눌러주세요.</Message>
+                <Form onSubmit={onFormSubmit}>
+                  <Form.Field>
+                    <label>File Upload</label>
+                    <Button as="label" htmlFor="file" type="button" animated="fade">
+                      <Button.Content visible>
+                        <Icon name="file" />
+                      </Button.Content>
+                      <Button.Content hidden>Choose a File</Button.Content>
+                    </Button>
+                    <input type="file" id="file" hidden onChange={fileChange} />
+                    <Button style={{ marginTop: '20px' }} type="submit" id="Download" onClick={fileDownload}>
+                      Download
+                    </Button>
+                  </Form.Field>
+                </Form>
+                <div>
+                  <canvas id="canvasInput"> input</canvas>
+                </div>
+              </Tab.Pane>
+            ),
+          },
+        ]}
+      />
+    </Segment>
+  );
+};
 
 export default Uploader;
