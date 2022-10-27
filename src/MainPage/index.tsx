0bfd60eb2805b8db50f8736a0876df2b65b4c5c3
Evan Moon <bboydart91@gmail.com>
2022-10-27 23:35:48 +0900

    feat: Aside 메뉴 추가 (#16)

--- src/MainPage/index.tsx
+++ src/MainPage/index.tsx
@@ -4,14 +4,7 @@ import { Stack } from 'quantumic-design';
 import Category from './Category';
 import { useEffect } from 'react';
 import { logger } from '@lubycon/logger';
-import { currencyName } from 'data/currencies/constants';
-
-const currencyPages = Object.entries(currencyName).map(([ticker, name]) => {
-  return {
-    name: `${name} 환율`,
-    link: `/currencies/${ticker}`,
-  };
-});
+import { pages } from 'common/constants/pages';
 
 const MainPage = () => {
   useEffect(() => {
@@ -28,33 +21,9 @@ const MainPage = () => {
           width: 100%;
         `}
       >
-        <Category
-          title="실생활 계산기"
-          links={[
-            { name: '연봉 계산기', link: '/salary-calculator' },
-            { name: '성장률 계산기', link: '/growth-rate' },
-            { name: '복리 계산기', link: '/compound-interest' },
-          ]}
-        />
-        <Category title="단위 계산기" links={[{ name: '평수 계산기', link: '/korean-area' }]} />
-        <Category title="실시간 환율" links={currencyPages} />
-        <Category
-          title="금융 정보"
-          links={[
-            {
-              name: '실시간 한미 금리차',
-              link: '/korea-usa-interest-rate',
-            },
-            {
-              name: '실시간 미국채 금리',
-              link: '/us-treasury-rate',
-            },
-            {
-              name: '퀀트 데이터',
-              link: '/quant-data',
-            },
-          ]}
-        />
+        {pages.map(({ category, links }) => (
+          <Category key={category} title={category} links={links} />
+        ))}
       </Stack>
     </Layout>
   );
---
+++ src/common/constants/pages.ts
@@ -0,0 +1,48 @@
+import { currencyName } from 'data/currencies/constants';
+
+const currencyPages = Object.entries(currencyName).map(([ticker, name]) => {
+  return {
+    name: `${name} 환율`,
+    link: `/currencies/${ticker}`,
+  };
+});
+
+interface Page {
+  category: string;
+  links: Array<{ name: string; link: string }>;
+}
+export const pages: Page[] = [
+  {
+    category: '실생활 계산기',
+    links: [
+      { name: '연봉 계산기', link: '/salary-calculator' },
+      { name: '성장률 계산기', link: '/growth-rate' },
+      { name: '복리 계산기', link: '/compound-interest' },
+    ],
+  },
+  {
+    category: '단위 계산기',
+    links: [{ name: '평수 계산기', link: '/korean-area' }],
+  },
+  {
+    category: '실시간 환율',
+    links: [...currencyPages],
+  },
+  {
+    category: '금융 정보',
+    links: [
+      {
+        name: '실시간 한미 금리차',
+        link: '/korea-usa-interest-rate',
+      },
+      {
+        name: '실시간 미국채 금리',
+        link: '/us-treasury-rate',
+      },
+      {
+        name: '퀀트 데이터',
+        link: '/quant-data',
+      },
+    ],
+  },
+];
