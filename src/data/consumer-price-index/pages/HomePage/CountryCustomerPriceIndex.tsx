78a4d9832cb9c1769312e6d8ab5de921df75014d
DongHyun Hwang <dhhwang@toss.im>
2022-11-18 00:41:37 +0900

    feat: color 값 변경
     Refs:

--- src/data/consumer-price-index/pages/HomePage/CountryCustomerPriceIndex.tsx
+++ src/data/consumer-price-index/pages/HomePage/CountryCustomerPriceIndex.tsx
@@ -1,4 +1,5 @@
 import { useMobileScreen } from 'common/hooks/useMobileScreen';
+import colors from 'open-color';
 import { Stack, Txt } from 'quantumic-design';
 
 interface Props {
@@ -11,11 +12,18 @@ interface Props {
 const CountryCustomerPriceIndex = ({ title, previous, real, prediction, announceDate }: Props) => {
   const isMobile = useMobileScreen();
 
+  const 이전값 = Number(previous?.replaceAll('%', ''));
+  const 현재값 = Number(real?.replaceAll('%', ''));
+  const 예측값 = Number(prediction?.replaceAll('%', ''));
+
+  const diff = 현재값 - 예측값;
+  const interestRateSpreadTextColor = diff > 0 ? colors.red[5] : colors.blue[5];
+
   return (
     <Stack gutter={8} direction="column" align="center">
       <Txt size={isMobile ? 14 : 16}>{title}</Txt>
-      <Txt>이전값: {previous}</Txt>
-      <Txt size={isMobile ? 18 : 24} weight={600}>
+      <Txt>이전값: {`${이전값}%`}</Txt>
+      <Txt size={isMobile ? 18 : 24} weight={600} color={interestRateSpreadTextColor}>
         {real}
       </Txt>
       <Txt>예측값(가이던스): {prediction}</Txt>
