ded359bbec8459279ce92fad46ac0f67dec85955
evan-moon <bboydart91@gmail.com>
2025-05-05 16:56:54 +0900

    add google ads sdk

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -38,6 +38,11 @@ class MyDocument extends Document {
             crossOrigin="anonymous"
             href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.5/dist/web/static/pretendard.css"
           />
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4113746985528013"
+            crossOrigin="anonymous"
+          ></script>
         </Head>
         <body>
           <Main />
